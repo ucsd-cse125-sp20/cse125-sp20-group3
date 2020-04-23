38e3feb658c47b946b41e8ea5137cf58dfa4d8fa
Charles Li <charlespli@sbcglobal.net>
2020-04-22 18:58:43 -0700

    small changes to player and server_main

--- common/player.cpp
+++ common/player.cpp
@@ -1,31 +1,53 @@
 #include "player.h"
 
-Player::Player(std::string objFilename) : OBJObject(objFilename) {
-        Player();
+Player::Player(mat4 model_mat, std::string objFilename) : Transform(model_mat) {
+    //TODO create a geometry
+	velocity_x = 0.f;
+	velocity_z = 0.f;
+	lastTime = std::chrono::steady_clock::now();
 }
 
-Player::Player() {
-        velocity_x = 0.f;
-        velocity_z = 0.f;
-        lastTime = glfwGetTime();
-        //lastTime = std::chrono::system_clock::now();
-}
-
-void Player::updateVelocity(float vel_x, float vel_z) {
-        velocity_x = vel_x;
-        velocity_z = vel_z;
+Player::Player(mat4 model_mat) : Transform(model_mat) {
+	velocity_x = 0.f;
+	velocity_z = 0.f;
+	lastTime = std::chrono::steady_clock::now();
 }
 
 void Player::update() {
-        //std::chrono::duration<float, curTime_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(curTime.time_since_epoch()).count();
-        float deltaTime = glfwGetTime() - lastTime;
-        lastTime = glfwGetTime();
-		glm::vec3 p = this->model * glm::vec4(0, 0, 0, 1);
-		std::cout << "player: " << p.x << " " << p.y << " " << p.z << std::endl;
-		/*if (velocity_x > 0.01f || velocity_x < -0.01f) {
-			pos_x = (velocity_x * deltaTime) + pos_x;
-		}
-		if (velocity_z > 0.01f || velocity_z < -0.01f) {
-			pos_z = (velocity_z * deltaTime) + pos_z;
-		}*/
+	//should only execute on the server
+	auto currTime = std::chrono::steady_clock::now();
+	std::chrono::duration<float> deltaDuration = currTime - lastTime;
+	float deltaTime = deltaDuration.count();
+	lastTime = std::chrono::steady_clock::now();
+
+	if (velocity_x > 0.01f || velocity_x < -0.01f) {
+		float x_translate = velocity_x * deltaTime;
+	}
+	if (velocity_z > 0.01f || velocity_z < -0.01f) {
+		float z_translate = velocity_z * deltaTime;
+	}
+
+	//TODO translate model by (x_translate, 0, z_translate)
+
+	//TODO handle rotation stuff
+}
+
+void Player::setVelocity(float vel_x, float vel_z) {
+	velocity_x = vel_x;
+	velocity_z = vel_z;
+}
+
+void Player::setMove(int move_x, int move_z) {
+	if (move_x < 0) velocity_x = -1 * MOVE_SPEED;
+	else if (move_x > 0) velocity_x = MOVE_SPEED;
+	else velocity_x = 0;
+
+	if (move_z < 0) velocity_z = -1 * MOVE_SPEED;
+	else if (move_z > 0) velocity_z = MOVE_SPEED;
+	else velocity_z = 0;
+}
+
+void Player::setPosRot(float pos_x, float pos_z, float rot_y) {
+	//should only execute on the client
+	//TODO update matrix accordingly
 }
--- common/player.h
+++ common/player.h
@@ -1,22 +1,25 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
-#include "OBJObject.h"
+#include "../client/src/Transform.h" //only include if client-side?
 #include <iostream>
 #include <chrono>
 #include <ctime>
 
-class Player : public OBJObject {
+#define MOVE_SPEED 5
+
+class Player : public Transform {
 private:
-        float lastTime;
-        float velocity_x, velocity_z;
-		float pos_x, pos_z;
+    std::chrono::steady_clock::time_point lastTime;
+    float velocity_x, velocity_z;
 public:
-        Player(std::string objFilename);
-        Player();
-		void update();
-        void updateVelocity(float vel_x, float vel_z);
-        void buildTower();
-        void spawnUnit();
+    Player(mat4 model_mat, std::string objFilename); //client-side, create geometry to load model
+    Player(mat4 model_mat); //server-side, only init state management logic
+	void update(); //server-side state management
+	void setVelocity(float vel_x, float vel_z);
+	void setMove(int move_x, int move_z);
+    void setPosRot(float pos_x, float pos_z, float rot_y); //client-side setting model to match server
+    void buildTower();
+    void spawnUnit();
 };
 #endif

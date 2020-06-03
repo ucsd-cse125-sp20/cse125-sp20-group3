fe3016e53c5eba24b6edae978a334bf5115710b6
Hao Luo <hal384@ucsd.edu>
2020-06-03 00:27:50 -0700

    super minion movement collision removed

--- common/superminion.cpp
+++ common/superminion.cpp
@@ -11,6 +11,65 @@ SuperMinion::SuperMinion(GameObjectData data, int id, Team* t, SceneManager_Serv
 	}
 }
 
+void SuperMinion::move(float deltaTime) {
+	if (doneMoving) return; //small optimization
+
+	actionState = ACTION_STATE_MOVE;
+
+	float remaining_move_dist = velocity * deltaTime; //full movement distance this minion travels this tick
+
+	while (remaining_move_dist > 0) { //while this minion still has distance to move
+		vec3 dest_vec = destNode->getPosition() - this->getPosition(); //vector to reach destNode
+		float dest_dist = length(dest_vec); //distance between this and destNode
+		float move_dist = std::min(remaining_move_dist, dest_dist); //this iteration, move full move_dist, or only to destNode if closer
+		vec3 move_vec = move_dist > 0 ? normalize(dest_vec) * move_dist : vec3(0); //move calculated distance along vector to destination
+
+		mat4 oldModel = model; //super minions ignore movement collision
+
+		/*auto other = ObjectDetection::getNearestObject(this, DETECTION_FLAG_COLLIDABLE, 1);
+		if (other) {
+			vec3 away = getPosition() - other->getPosition();
+			float dist = length(away);
+			if (dist < 1.0f) {
+				//print(away);
+				vec3 perp = vec3(-away[2], 0.f, -away[0]);
+				if (abs(dot(perp, move_vec)) < 0.001f) {
+					away += MathUtils::randfUniform(-1, 1) * perp;
+				}
+				move_vec += move_dist * (0.5f / max(dist, 0.5f)) * normalize(away);
+				move_vec = move_dist * normalize(move_vec);
+			}
+		}*/
+
+		remaining_move_dist -= move_dist;
+
+		model[3][0] += move_vec[0];
+		model[3][2] += move_vec[2];
+		vec3 forward = vec3(model[3][0] - oldModel[3][0], 0, model[3][2] - oldModel[3][2]);
+		if (length(forward) > 0.01f) {
+			forward = normalize(forward);
+			vec3 right = cross(forward, vec3(0, 1, 0));
+			model[0] = vec4(right, 0);
+			model[2] = vec4(-forward, 0);
+		}
+
+
+		if (length(this->getPosition() - destNode->getPosition()) < 0.5f) { //reached destNode with this iteration
+			PathNode* nextNode; //continue moving to the next node
+			if (this->team->teamColor == RED_TEAM) nextNode = this->destNode->next_red;
+			else nextNode = this->destNode->next_blue;
+
+			if (nextNode != nullptr) this->destNode = nextNode;
+			else {
+				std::cout << "minion reached the end of the path!\n";
+				doneMoving = true;
+				actionState = ACTION_STATE_IDLE;
+				break;
+			}
+		}
+	}
+}
+
 void SuperMinion::dropPickups() {
 	srand((unsigned int)time(NULL));
 	vec3 pos = this->getPosition();
